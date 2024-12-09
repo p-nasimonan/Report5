dc4d0da3f749ef3c4d67ed7fae9c761cdbdfa4fa
p-nasi <pnasimonan@gmail.com>
2024-12-09 11:19:41 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245719/NullTest.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e245719;
+public class NullTest {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
