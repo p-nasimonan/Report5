d2b65a0190e23cd4e39df502c6cd0dc0c9c61fab
p-nasi <pnasimonan@gmail.com>
2024-12-10 12:19:23 +0900

    例外処理

--- app/src/main/java/jp/ac/uryukyu/ie/e245719/NullTest.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245719/NullTest.java
@@ -1,7 +1,12 @@
 package jp.ac.uryukyu.ie.e245719;
 public class NullTest {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
